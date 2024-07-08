c94437456391c13c5761ce313f4546ce0e5d9121
Renamekk <black80512@gmail.com>
2024-07-08 18:43:17 +0200

    goit-js-hw-08

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const textInput = document.querySelector('#name-input');
-const textoutput = document.querySelector('#name-output');
-
-textInput.addEventListener('input', event => {
-  event.currentTarget.value.trim() === ''
-    ? (textoutput.textContent = 'Anonymous')
-    : (textoutput.textContent = event.currentTarget.value.trim());
-});
